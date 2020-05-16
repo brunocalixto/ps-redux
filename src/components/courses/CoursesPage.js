f8444d20fe18053d9cd7a68f26e500031e6b68a3
Bruno Calixto da Silva <bruno.calixto@b2wdigital.com>
2020-05-16 16:58:35 -0300

    add: new page component

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  handleChange = event => {
+    const course = { ...this.course, title: event.target.value };
+    this.setState({ course: course });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
+  }
+}
+
+export default CoursesPage;
