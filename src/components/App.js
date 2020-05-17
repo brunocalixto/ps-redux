23255d40667f019f72ae354a56db0f23c460e84d
Bruno Calixto da Silva <bruno.calixto@b2wdigital.com>
2020-05-17 10:58:25 -0300

    add: server json and scripts api

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,22 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
-import CoursesPage from "./courses/CoursesPage";
 import Header from "./common/Header";
 import PageNotFound from "./PageNotFound";
+import CoursesPage from "./courses/CoursesPage";
 
-const App = () => (
-  <div className="container-fluid">
-    <Header />
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/about" component={AboutPage} />
-      <Route exact path="/courses" component={CoursesPage} />
-      <Route component={PageNotFound} />
-    </Switch>
-  </div>
-);
+function App() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
+  );
+}
 
 export default App;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,7 +4,8 @@ import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 
 export default function configureStore(initialState) {
   const composeEnhancers =
-    window.__REDUX__DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add suport for Redux dev tools
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+
   return createStore(
     rootReducer,
     initialState,
